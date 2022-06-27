bd967c8422a48aee4ea166b3c83dafa249798836
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-06-27 18:45:58 -0500

    first commit

---
+++ src/main/java/com/jonfriend/java19daikichipathvariables/Java19daikichipathvariablesApplication.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java19daikichipathvariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java19daikichipathvariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java19daikichipathvariablesApplication.class, args);
+	}
+
+}
